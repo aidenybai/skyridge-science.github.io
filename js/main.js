2dbf77f02321cfe6fb0ef2359a2ca03a078e2608
Aiden Bai <35048878+innerjoker@users.noreply.github.com>
2018-09-15 16:09:37 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -17,11 +17,38 @@ document.onkeydown = function(e) {
     }      
  }
 
-window.addEventListener("keydown", function(e) {
-    // space, page up, page down and arrow keys:
-    if([32, 33, 34, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
-        e.preventDefault();
+function webLoad() {
+    function a() {
+        $(window).scrollTop() >= 50 ? e.addClass("scrolled") : e.removeClass("scrolled")
+        $(window).scrollTop() >= 50 ? e.addClass("scrolled") : e.removeClass("scrolled")
     }
-}, false);
+
+    var t = $(".navbar-toggler"),
+        o = null;
+    $("#main-header").on("click", t, function(a) {
+        if (o = $(a.target), $("body").removeClass("mainmenu-open"), o.hasClass("collapsed")) return $("body").addClass("mainmenu-open")
+    }), $("body").on("click", "#headerMask", function(a) {
+        a.preventDefault(), o.click(), $("#main-header .collapse").collapse("hide")
+    });
+    new ScrollTrigger({
+        toggle: {
+            visible: "scroll-show",
+            hidden: "scroll-hide"
+        },
+        once: !1
+    });
+    var e = $("#main-header-inner");
+    var e = $("#main-header-inner");
+    a(), $(window).scroll(a), $(document).on("click", "[data-toggle='lightbox']", function(a) {
+        a.preventDefault(), $(this).ekkoLightbox()
+    });
+}
+$("body").removeClass("nojs"), window.web = {}, window.web.dataCookies = !1, window.web.dataConsent = !1, window.web.dataTrack = !1, window.web.dataUA = !1, document.cookie.split(";").filter(function(a) {
+    return a.indexOf("consent=yes") >= 0
+}).length && (window.web.dataConsent = !0, $("html").addClass("data-consent")), document.cookie.split(";").filter(function(a) {
+    return a.indexOf("consent=no") >= 0
+}).length && (window.web.dataConsent = !1, $("html").addClass("data-no-consent")), navigator && (navigator.cookieEnabled && (window.web.dataCookies = !0, $("html").addClass("data-cookies")), !navigator.doNotTrack && window.web.dataConsent && (window.web.dataTrack = navigator.userAgent, $("html").addClass("data-track")), navigator.userAgent && (window.web.dataUA = navigator.userAgent)), window.addEventListener("load", webLoad, !1);
+
+
 
 document.addEventListener('contextmenu', event => event.preventDefault());
