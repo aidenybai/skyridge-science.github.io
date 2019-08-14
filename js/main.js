a972d98d0e71e2ee93edf87e27d72c760ac52bb0
Aiden Bai <mail.contraband@gmail.com>
2019-08-14 10:54:33 -0700

    Fix navigation bar background onscroll

--- js/main.js
+++ js/main.js
@@ -15,3 +15,46 @@ $(document).ready(function() {
     }
   });
 });
+
+function webLoad() {
+  function a() {
+    $(window).scrollTop() >= 50
+      ? e.addClass("scrolled")
+      : e.removeClass("scrolled");
+    $(window).scrollTop() >= 50
+      ? e.addClass("scrolled")
+      : e.removeClass("scrolled");
+  }
+
+  var t = $(".navbar-toggler"),
+    o = null;
+  $("#main-header").on("click", t, function(a) {
+    if (
+      ((o = $(a.target)),
+      $("body").removeClass("mainmenu-open"),
+      o.hasClass("collapsed"))
+    )
+      return $("body").addClass("mainmenu-open");
+  }),
+    $("body").on("click", "#headerMask", function(a) {
+      a.preventDefault(),
+        o.click(),
+        $("#main-header .collapse").collapse("hide");
+    });
+  new ScrollTrigger({
+    toggle: {
+      visible: "scroll-show",
+      hidden: "scroll-hide"
+    },
+    once: !1
+  });
+  var e = $("#main-header-inner");
+  var e = $("#main-header-inner");
+  a(),
+    $(window).scroll(a),
+    $(document).on("click", "[data-toggle='lightbox']", function(a) {
+      a.preventDefault(), $(this).ekkoLightbox();
+    });
+}
+
+window.addEventListener("load", webLoad, !1);
